ee9f21ace4ea84c90532ecf73934099616cccb74
ezekiel-Kaeyros <144704603+ezekiel-Kaeyros@users.noreply.github.com>
2024-05-08 14:16:02 +0100

    Update constants.ts

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:4000"
-// export const BASE_URL = "https://back-end.kaeyros.shop"
+// export const BASE_URL = "http://localhost:4000"
+export const BASE_URL = "https://back-end.kaeyros.shop"
 
 export const AGENT_USEQUERY_KEY = "allAgentsKey";
 export const CHANNEL_CLUSTER_USEQUERY_KEY = "allChannelClustersKey";
